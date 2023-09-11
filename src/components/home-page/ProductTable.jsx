536e93dd4711bd5df0d468592f3dcca265fc3ba4
Selena <selena.k.rodriguez@gmail.com>
2023-09-10 20:00:57 -0500

    took products from api and maped them into divs

--- src/components/home-page/ProductTable.jsx
+++ src/components/home-page/ProductTable.jsx
@@ -1,3 +1,23 @@
-const ProductTable = () => {};
+import React from "react";
+import "../../styles/ProductTable.css";
+
+const ProductTable = ({ products }) => {
+  return (
+    <div id="product-section" className="sections">
+      {products.map((product) => (
+        <div key={product.id} id={`product-${product.id}`} className="products">
+          <img
+            className="product-image"
+            src={product.image}
+            alt={product.name}
+          ></img>
+          <div className="product-names">{product.name}</div>
+          {/* <div className="product-descriptions">{product.description}</div> */}
+          <div className="product-price">{`$${product.price}`}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ProductTable;
