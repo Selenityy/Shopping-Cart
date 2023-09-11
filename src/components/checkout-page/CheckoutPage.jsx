7ce6c9d6aa51168ee241512baf2d699eca64a2b6
Selena <selena.k.rodriguez@gmail.com>
2023-09-11 18:41:50 -0500

    added usenavigate to go back to the home page

--- src/components/checkout-page/CheckoutPage.jsx
+++ src/components/checkout-page/CheckoutPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const CheckoutPage = () => {
-  return <div>Checkout</div>;
+  const navigate = useNavigate();
+
+  const handleGoBackClick = () => {
+    navigate("/"); // Use the navigate function to go back to the home page ("/")
+  };
+
+  return (
+    <div>
+      <h2>Checkout Page</h2>
+      {/* Your checkout content */}
+      <button onClick={handleGoBackClick}>Go Back to Home</button>
+    </div>
+  );
 };
 
 export default CheckoutPage;
