3377c3770f118699a9ed9cbfb90c51aadbc5b5b8
Selena <selena.k.rodriguez@gmail.com>
2023-09-12 15:33:48 -0500

    moved the homeRef to the HomePage

--- src/components/home-page/Header.jsx
+++ src/components/home-page/Header.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import MiniCart from "./header-components/MiniCart";
 import PageSelections from "./header-components/PageSelections";
 import Icons from "./header-components/Icons";
+import "../../styles/Header.css";
 
-const Header = ({ scrollToAd, scrollToProducts }) => {
+const Header = ({ scrollToAd, scrollToProducts, scrollToHome }) => {
   const [isCheckoutVisible, setIsCheckoutVisible] = useState(false);
 
   const handleCartClick = () => {
@@ -16,6 +17,7 @@ const Header = ({ scrollToAd, scrollToProducts }) => {
       <PageSelections
         scrollToAd={scrollToAd}
         scrollToProducts={scrollToProducts}
+        scrollToHome={scrollToHome}
       />
       <Icons handleCartClick={handleCartClick} />
       <MiniCart
