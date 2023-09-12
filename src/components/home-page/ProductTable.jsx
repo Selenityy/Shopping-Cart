232f6cb3f2e80839f79567edc4a6ff9f7cfd7506
Selena <selena.k.rodriguez@gmail.com>
2023-09-12 18:07:19 -0500

    implemented subtract and add buttons to the quantity to update the product quantity property

--- src/components/home-page/ProductTable.jsx
+++ src/components/home-page/ProductTable.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Button from "../utilities/Button";
 import "../../styles/ProductTable.css";
 
 const ProductTable = ({ products, productRef }) => {
+  const productList = [...products];
+  console.log("Products:", productList);
+
+  const [productQuantity, setProductQuantity] = useState([]);
+
+  const handleMinusClick = (index) => {
+    const newProductQuantity = [...productQuantity];
+    if (newProductQuantity[index] > 0) {
+      newProductQuantity[index] -= 1;
+      setProductQuantity(newProductQuantity);
+    }
+  };
+
+  const handleAddClick = (index) => {
+    const newProductQuantity = [...productList];
+    newProductQuantity[index].quantity += 1;
+    setProductQuantity(newProductQuantity);
+  };
+
   return (
     <div ref={productRef} id="product-section" className="sections">
-      {products.map((product) => (
+      {products.map((product, index) => (
         <div key={product.id} id={`product-${product.id}`} className="products">
           <img
             className="product-image"
@@ -14,6 +34,26 @@ const ProductTable = ({ products, productRef }) => {
           <div className="product-names">{product.name}</div>
           {/* <div className="product-descriptions">{product.description}</div> */}
           <div className="product-price">{`$${product.price}`}</div>
+          <div>Qty</div>
+          <Button
+            id={`minus-qty-${index}`}
+            className="qty-btn"
+            onClick={() => handleMinusClick(index)}
+            label="-"
+          />
+          <div>{product.quantity}</div>
+          <Button
+            id={`add-qty-${index}`}
+            className="qty-btn"
+            onClick={() => handleAddClick(index)}
+            label="+"
+          />
+          <Button
+            id="add-to-cart"
+            className="qty-btn"
+            // onClick={increase selected items array by digit in input field}
+            label="Add to Cart"
+          />
         </div>
       ))}
     </div>
