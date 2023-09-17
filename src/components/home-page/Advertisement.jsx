5c6b806cfa77bd1c66f63ba4b77404b6d9c07209
Selena <selena.k.rodriguez@gmail.com>
2023-09-17 16:49:45 -0500

    added id's to the h2/h3

--- src/components/home-page/Advertisement.jsx
+++ src/components/home-page/Advertisement.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Button from "../utilities/Button";
+import "../../styles/Advertisement.css";
 
 const Advertisement = ({ adRef }) => {
   return (
     <div id="ad-section" className="sections">
-      <h2 ref={adRef}>New Arrivals</h2>
-      <h3>Find our newest collections here</h3>
+      <h2 id="ad-title" ref={adRef}>
+        New Arrivals
+      </h2>
+      <h3 id="ad-message">Find our newest collections here</h3>
       <Button
         id="shop-now-btn"
         className="shop-btn"
