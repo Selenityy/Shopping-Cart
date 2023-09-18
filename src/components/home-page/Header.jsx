d2610ac9b38ee92798775fca0d4b72b01297591a
Selena <selena.k.rodriguez@gmail.com>
2023-09-18 17:50:05 -0500

    Attempted to move mini cart to home page but decided to leave it in the header

--- src/components/home-page/Header.jsx
+++ src/components/home-page/Header.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
+import MiniCart from "./header-components/MiniCart";
 import PageSelections from "./header-components/PageSelections";
 import Icons from "./header-components/Icons";
 import "../../styles/Header.css";
 
-const Header = ({
-  scrollToAd,
-  scrollToProducts,
-  scrollToHome,
-  handleCartClick,
-}) => {
+const Header = ({ scrollToAd, scrollToProducts, scrollToHome }) => {
+  const [isCheckoutVisible, setIsCheckoutVisible] = useState(false);
+
+  const handleCartClick = () => {
+    setIsCheckoutVisible(!isCheckoutVisible);
+  };
+
   return (
     <div id="header-section" className="sections">
       <h1 id="brand-name">Amazela</h1>
@@ -19,6 +21,10 @@ const Header = ({
       />
       <div id="header-hr" className="horizontal-line"></div>
       <Icons handleCartClick={handleCartClick} />
+      <MiniCart
+        handleCartClick={handleCartClick}
+        isVisible={isCheckoutVisible}
+      />
     </div>
   );
 };
--- src/components/home-page/HomePage.jsx
+++ src/components/home-page/HomePage.jsx
@@ -1,20 +1,12 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import Advertisement from "./Advertisement";
 import Header from "./Header";
 import ProductTable from "./ProductTable";
-import MiniCart from "./header-components/MiniCart";
-import "../../styles/MiniCart.css";
 
 const HomePage = () => {
   const adRef = useRef(null);
   const productRef = useRef(null);
   const homeRef = useRef(null);
-
-  const [isCheckoutVisible, setIsCheckoutVisible] = useState(false);
-
-  const handleCartClick = () => {
-    setIsCheckoutVisible(!isCheckoutVisible);
-  };
 
   const scrollToAd = () => {
     if (adRef.current) {
@@ -41,11 +33,6 @@ const HomePage = () => {
         scrollToAd={scrollToAd}
         scrollToProducts={scrollToProducts}
         scrollToHome={scrollToHome}
-        handleCartClick={handleCartClick}
-      />
-      <MiniCart
-        handleCartClick={handleCartClick}
-        isVisible={isCheckoutVisible}
       />
       <Advertisement adRef={adRef} />
       <ProductTable productRef={productRef} />
