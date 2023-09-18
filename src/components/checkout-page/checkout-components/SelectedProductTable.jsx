a6e196a74360f8fa61c53fed07a38272fac98eda
Selena <selena.k.rodriguez@gmail.com>
2023-09-17 21:38:11 -0500

    grouped all h3s into a div for flexbox

--- src/components/checkout-page/checkout-components/SelectedProductTable.jsx
+++ src/components/checkout-page/checkout-components/SelectedProductTable.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ProductRow from "./ProductRow";
-import "../../../styles/Checkout.css"
+import "../../../styles/Checkout.css";
 
 const SelectedProductTable = () => {
-    return (
-        <div>
-            <h3 id="checkout-product-title">Product</h3>
-            <h3 id="checkout-quantity-title">Quantity</h3>
-            <h3 id="checkout-total-title">Total</h3>
-            <hr></hr>
-            <ProductRow />
-        </div>
-    )
+  return (
+    <div>
+      <div id="selected-product-table-h3s">
+        <h3 id="checkout-product-title">Product</h3>
+        <h3 id="checkout-quantity-title">Quantity</h3>
+        <h3 id="checkout-total-title">Total</h3>
+      </div>
+      <hr></hr>
+      <ProductRow />
+    </div>
+  );
 };
 
 export default SelectedProductTable;
